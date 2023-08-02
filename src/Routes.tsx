d11776e03c774060468791e2ba4324bac5141dd5
crazyads69 <leminhtri11a1@gmail.com>
2023-08-02 09:59:56 +0700

    Use index route and add HomePage

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ProductsPage } from './pages/ProductsPage';
 import { Header } from './Header';
 import App from './App';
+import { HomePage } from './pages/HomePage';
 import { ErrorPage } from './pages/ErrorPage';
 import { ProductPage } from './pages/ProductPage';
 const router = createBrowserRouter([
@@ -10,6 +11,7 @@ const router = createBrowserRouter([
         element: <App />,
         errorElement: <ErrorPage />,
         children: [
+            { index: true, element: <HomePage /> },
             { path: 'products', element: <ProductsPage /> },
             { path: 'products/:id', element: <ProductPage /> },
         ],
