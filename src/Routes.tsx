03a4a59c69ce8ed33b49a9fc2be39d867041b257
crazyads69 <leminhtri11a1@gmail.com>
2023-08-02 09:53:40 +0700

    Add error page and useRouteError()

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ProductsPage } from './pages/ProductsPage';
 import { Header } from './Header';
 import App from './App';
+import { ErrorPage } from './pages/ErrorPage';
 import { ProductPage } from './pages/ProductPage';
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
+        errorElement: <ErrorPage />,
         children: [
             { path: 'products', element: <ProductsPage /> },
             { path: 'products/:id', element: <ProductPage /> },
